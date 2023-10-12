299b9803ea8750769d2a28002e27d536d6f115ff
Greg Weber <gweber@digitalmint.io>
2023-10-12 10:33:57 -0500

    fix stack trace caller skip

--- structured_test.go
+++ structured_test.go
@@ -1,6 +1,10 @@
 package errors
 
 import (
+	"bytes"
+	"context"
+	"log/slog"
+	"strings"
 	"testing"
 )
 
@@ -21,7 +25,20 @@ func TestStructured(t *testing.T) {
 	if numAttrs := err.Record.NumAttrs(); numAttrs != 2 {
 		t.Errorf("expected 2 attributes, got %d for %s", numAttrs, err.Error())
 	}
-	if numAttrs := SlogRecord(err).NumAttrs(); numAttrs != 4 {
+	record := SlogRecord(err)
+	if numAttrs := record.NumAttrs(); numAttrs != 4 {
 		t.Errorf("expected 4 attributes, got %d for %s", numAttrs, err.Error())
+	}
+
+	// Test stack trace
+	hOpts := slog.HandlerOptions{
+		AddSource: true,
+	}
+	buf := new(bytes.Buffer)
+	if err := slog.NewTextHandler(buf, &hOpts).Handle(context.Background(), *record); err != nil {
+		t.Fatalf("error writing out record %+v", err)
+	}
+	if !strings.Contains(buf.String(), "structured_test.go") {
+		t.Errorf("expected stack trace with file")
 	}
 }
