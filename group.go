c3f6d6a0fc99004bdd4814f499a9c20fc37c69d8
Greg Weber <gweber@digitalmint.io>
2024-06-19 12:53:14 -0500

    group Errors support Unwrap() []error

--- group.go
+++ group.go
@@ -2,25 +2,27 @@ package errors
 
 // ErrorGroup is an interface for multiple errors that are not a chain.
 // This happens for example when executing multiple operations in parallel.
+// Also can now define Unwraps() []error
 type ErrorGroup interface {
 	Errors() []error
 }
 
-// If the error is an ErrorGroup, return its list of errors
-// If it is not an error group, the list will contain just itself.
-// If nil return nil
+type unwraps interface {
+	Unwrap() []error
+}
+
+// If the error is not nil and an ErrorGroup or satisfies Unwraps() []error, return its list of errors
+// otherwise return nil
 func Errors(err error) []error {
 	if err == nil {
 		return nil
 	}
 	if group, ok := err.(ErrorGroup); ok {
-		errors := group.Errors()
-		result := make([]error, len(errors))
-		copy(result, errors)
-		return result
-	} else {
-		return []error{err}
+		return group.Errors()
+	} else if group, ok := err.(unwraps); ok {
+		return group.Unwrap()
 	}
+	return nil
 }
 
 // WalkDeep does a depth-first traversal of all errors.
@@ -38,8 +40,8 @@ func WalkDeep(err error, visitor func(err error) bool) bool {
 	}
 
 	// Go wide
-	if group, ok := err.(ErrorGroup); ok {
-		for _, err := range group.Errors() {
+	if errors := Errors(err); len(errors) > 0 {
+		for _, err := range errors {
 			if early := WalkDeep(err, visitor); early {
 				return true
 			}
