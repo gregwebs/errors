b83a80a8a08d382719eef1f47289e64cc825af61
Greg Weber <greg@gregweber.info>
2024-12-18 21:08:53 -0600

    modernize the package overview

--- errors.go
+++ errors.go
@@ -1,46 +1,15 @@
-// Package errors provides simple error handling primitives.
+// Package errors provides error handling primitives that add stack traces and metadata to errors.
 //
-// The traditional error handling idiom in Go is roughly akin to
+// Key Concepts
+// ----------------
 //
-//	if err != nil {
-//	        return err
-//	}
-//
-// which applied recursively up the call stack results in error reports
-// without context or debugging information. The errors package allows
-// programmers to add context to the failure path in their code in a way
-// that does not destroy the original value of the error.
-//
-// # Adding context to an error
-//
-// The errors.Wrap function returns a new error that adds context to the
-// original error by recording a stack trace at the point Wrap is called,
-// and the supplied message. For example
-//
-//	_, err := ioutil.ReadAll(r)
-//	if err != nil {
-//	        return errors.Wrap(err, "read failed")
-//	}
-//
-// If additional control is required the errors.AddStack and errors.WithMessage
-// functions destructure errors.Wrap into its component operations of annotating
-// an error with a stack trace and an a message, respectively.
-//
-// # Retrieving the cause of an error
-//
-// Using errors.Wrap constructs a stack of errors, adding context to the
-// preceding error. Depending on the nature of the error it may be necessary
-// to reverse the operation of errors.Wrap to retrieve the original error
-// for inspection. Any error value which implements the Unwrap interface
-// can be inspected one level deeper by the errors.Unwrap function. errors.Cause will recursively unwrap
-// the error. For example:
-//
-//	switch err := errors.Cause(err).(type) {
-//	case *MyError:
-//	        // handle specifically
-//	default:
-//	        // unknown error
-//	}
+// *   **Adding Stack traces**: All the error creation and wrapping functions ensure a stack trace is recorded for the error.
+// *   **Adding Context**: The `errors.Wrap` and `errors.Wrapf` functions adds an additional string context to an error.
+// *   **Adding Structured data**: The `errors.Wraps` and `errors.Slog` functions adds structured data to errors.
+// *   **Formatted Printing**: Errors returned from this package implement the `fmt.Formatter` interface- verbose printing options will show the stack trace.
+// *   **Retrieving underlying errors**: In addition to standard `errors.Unwrap`, `errors.Is`, and `errors.As`, there are `errors.AsType`, `errors.Cause`, and `errors.UnwrapGroups`.
+// *   **Retrieving the Stack Trace**: `errors.GetStackTracer` retrieves the stack trace from the error.
+// *   **Retrieving the structured data**: `errors.SlogRecord` retrieves structured data as an slog.Record.
 //
 // # Formatted printing of errors
 //
@@ -53,28 +22,6 @@
 //	%+v   extended format. Each Frame of the error's StackTrace will
 //	      be printed in detail.
 //	%-v   similar to %s but newline separated. No stack traces included.
-//
-// # Retrieving the stack trace of an error or wrapper
-//
-// New, Errorf, Wrap, and Wrapf record a stack trace at the point they are invoked.
-// This information can be retrieved with the StackTracer interface that returns
-// a StackTrace. Where errors.StackTrace is defined as
-//
-//	type StackTrace []Frame
-//
-// The Frame type represents a call site in the stack trace. Frame supports
-// the fmt.Formatter interface that can be used for printing information about
-// the stack trace of this error. For example:
-//
-//	if stacked := errors.GetStackTracer(err); stacked != nil {
-//	        for _, f := range stacked.StackTrace() {
-//	                fmt.Printf("%+s:%d", f)
-//	        }
-//	}
-//
-// See the documentation for Frame.Format for more details.
-//
-// errors.Find can be used to search for an error in the error chain.
 package errors
 
 import (
